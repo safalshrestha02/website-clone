3e3e9f271079d0b4f8e6653bf3a8fb0c34a66da7
Safal Shrestha <shresthasafal000@gmail.com>
2023-12-13 16:06:18 +0545

    feat: added css to cta banner and pricing section

--- info-clone/components/CtaBanner.jsx
+++ info-clone/components/CtaBanner.jsx
@@ -1,21 +1,52 @@
+/* eslint-disable react/no-unknown-property */
 export default function CtaBanner() {
   return (
-    <>
+    <div className="cta-banner">
       <div className="get-started">
         <div className="get-started-left">
           <h2>Ready to Get Started?</h2>
-          <p>
+          <p className="cta-banner-upper-text">
             Embark on Your Journey to Limitless Potential with a Simple Click
           </p>
-          <button>Get infoGPT</button>
-          <p>
+          <button className="cta-banner-btn">
+            <span>Get infoGPT</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 20 21"
+              fill="none"
+            >
+              <path
+                d="M11.6667 4.66675L17.5 10.5001M17.5 10.5001L11.6667 16.3334M17.5 10.5001H2.5"
+                stroke="url(#paint0_linear_328_2531)"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></path>
+              <defs>
+                <linearGradient
+                  id="paint0_linear_328_2531"
+                  x1="1.8421"
+                  y1="4.66675"
+                  x2="20.5631"
+                  y2="7.80461"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stop-color="#9024EC"></stop>
+                  <stop offset="1" stop-color="#5F0CF9"></stop>
+                </linearGradient>
+              </defs>
+            </svg>
+          </button>
+          <p className="cta-banner-lower-text">
             Limited Time Offer – Act Now! and get unlimited access to infoGPT
           </p>
         </div>
         <div className="get-started-right">
-          <img src="" alt="image" />
+          <img src="/banner-image.png" alt="image" />
         </div>
       </div>
-    </>
+    </div>
   );
 }
--- info-clone/components/Pricing.jsx
+++ info-clone/components/Pricing.jsx
@@ -1,14 +1,15 @@
+/* eslint-disable react/no-unknown-property */
 /* eslint-disable react/no-unescaped-entities */
 export default function Pricing() {
   return (
-    <>
+    <div className="pricing-section">
       <div className="pricing">
         <div className="pricing-heading">
-          <p>Pricing</p>
+          <p className="pricing-top-text">Pricing</p>
           <h2>
             Unlock <span>Unlimited</span> Access with Early Bird Pricing
           </h2>
-          <p>
+          <p className="pricing-bot-text">
             Be among the first to experience the future with infoGPT's Early
             Bird Pack
           </p>
@@ -22,26 +23,112 @@ export default function Pricing() {
               whenever you need it.
             </p>
             <div className="pricing-included">
-              <p>What's included</p>
+              <h3>What's included</h3>
               <div className="included-items">
-                <p>All infoGPT Features</p>
-                <p>Unlimited Requests, No Limits</p>
-                <p>GPT-4 Powered Assistant</p>
-                <p>Priority support</p>
+                <div className="included-items-list-1">
+                  <p>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                      class="flex-none w-5 h-6 text-indigo-600"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    All infoGPT Features
+                  </p>
+                  <p>
+                    {" "}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                      class="flex-none w-5 h-6 text-indigo-600"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    Unlimited Requests, No Limits
+                  </p>
+                </div>
+                <div className="included-items-list-2">
+                  <p>
+                    {" "}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                      class="flex-none w-5 h-6 text-indigo-600"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    GPT-4 Powered Assistant
+                  </p>
+                  <p>
+                    {" "}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                      class="flex-none w-5 h-6 text-indigo-600"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    Priority support
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="pricing-card-right-outside">
+            <div className="pricing-card-right">
+              <div className="pricing-card-text">
+                <p className="pricing-para-1">Pay once, own it forever</p>
+                <p className="pricing-para-2">
+                  <span className="pricing-span-1">$10</span>
+                  <span className="pricing-span-2">USD</span>
+                </p>
+                <button className="pricing-btn">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="w-4 h-4 mr-1"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="2"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M8 11V7a4 4 0 118 0m-4 8v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2z"
+                    ></path>
+                  </svg>
+                  Unlock Unlimited Access
+                </button>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div className="pricing-button">
-        <p>Pay once, own it forever</p>
-        <p>
-          <span>$10</span> USD
-        </p>
-        <button>
-          <img src="" alt="lock"></img>Unlock Unlimited Access
-        </button>
-      </div>
-    </>
+    </div>
   );
 }
